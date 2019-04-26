2405b367d372d119dacc64556af2ec698579eca7
Brian Hayes <bjoeyhayes@gmail.com>
2019-04-26 14:12:59 -0400

    redux - prep gallery files

---
+++ src/redux/actiontypes/gallery.js
@@ -0,0 +1,11 @@
+const ADD_IMAGES = 'gallery/ADD_IMAGES';
+const REMOVE_IMAGES = 'gallery/REMOVE_IMAGES';
+const EDIT_IMAGE = 'gallery/EDIT_IMAGE';
+const MARK_IMAGE_AS_EDITED = 'gallery/MARK_IMAGE_AS_EDITED';
+
+export {
+  ADD_IMAGES,
+  REMOVE_IMAGES,
+  EDIT_IMAGE,
+  MARK_IMAGE_AS_EDITED
+};
