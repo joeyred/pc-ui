0d9e5111ceca08c1dd11e0c15ddff1b8b33a3ce3
Brian Hayes <bjoeyhayes@gmail.com>
2019-05-14 19:42:31 -0400

    Add Counter component for adding items to cart

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,45 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Icon from '../Icon';
 
 import styles from './Counter.module.scss';
 
-const Counter = (props) => {
-  const {
-    updateCount,
-    count
-  } = props;
-  return (
-    <div className={styles.container}>
-      <button
-        className={`${styles.button}`}
-        onClick={() => updateCount()}
-      >
-        <Icon name='Remove' />
-      </button>
-      <div className={styles.count}>
-        {count}
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+  static defaultProps = {
+    count: 0,
+    updateCountHandler: false,
+  };
+
+  static propTypes = {
+    updateCount: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]),
+    count: PropTypes.number,
+    id: PropTypes.string
+  };
+
+  componentDidMount() {
+    const { count } = this.props;
+    this.setState({ count });
+  }
+
+  updateCount(direction) {
+    const {
+      updateCountHandler,
+    } = this.props;
+    let count = updateCountHandler ? this.props.count : this.state.count;
+    if (direction === 'increase') {
+      count++
+    }
+    if (direction === 'decrease' && count !== 0) {
+      count--
+    }
+
+    if (updateCountHandler) {
+      updateCountHandler(count);
+    } else {
+      this.setState({ count });
+    }
+  }
+
+  render() {
+    const {
+      updateCountHandler,
+    } = this.props;
+    const count = updateCountHandler ? this.props.count : this.state.count;
+
+    return (
+      <div className={styles.container}>
+        <button
+          className={`${styles.button}`}
+          onClick={() => this.updateCount('decrease')}
+        >
+          <Icon name='Remove' />
+        </button>
+        <div className={styles.count}>
+          {count}
+        </div>
+        <button
+          className={`${styles.button}`}
+          onClick={() => this.updateCount('increase')}
+        >
+          <Icon name='Add' />
+        </button>
       </div>
-      <button
-        className={`${styles.button}`}
-        onClick={() => updateCount()}
-      >
-        <Icon name='Add' />
-      </button>
-
-    </div>
-  );
+    );
+  }
 }
-
-Counter.defaultProps = {
-  count: 0
-}
-
-Counter.propTypes = {
-  updateCount: PropTypes.func.isRequired,
-  count: PropTypes.number,
-  id: PropTypes.string
-};
 
 export default Counter;
